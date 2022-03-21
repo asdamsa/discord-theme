9adbfd34d6f5a4f693b609b17731c1ae817d4f4a
bomzamkza999 <48094894+bomzamkza999@users.noreply.github.com>
2022-03-21 16:07:16 +0700

    Delete videotest.php

--- videotest.php
+++
@@ -1,2 +0,0 @@
-<video autoplay="autoplay" loop="loop" controls="controls" width="1920" height="1080">
-<source src="https://asdamsa.github.io/discord-theme/2022-03-21%2015-09-26.mp4" type="video/mp4"></video>
