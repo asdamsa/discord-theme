cc80eb0c7b8cece2e967dd66c55306ecbbb11641
bomzamkza999 <48094894+bomzamkza999@users.noreply.github.com>
2022-03-21 16:01:52 +0700

    Create videotest.php

---
+++ videotest.php
@@ -0,0 +1,2 @@
+<video autoplay="autoplay" loop="loop" controls="controls" width="1920" height="1080">
+<source src="https://asdamsa.github.io/discord-theme/2022-03-21%2015-09-26.mp4" type="video/mp4"></video>
